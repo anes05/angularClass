85f3821a32a4cfc936a1581eccfada80974c2d0d
anes05 <anesmsaddak5@gmail.com>
2024-02-12 16:15:02 +0100

    getting the param from the url

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  id!:number;
+  //oinjecter le service avtivated Route dans ce service sous le nom activatedRoute
+  constructor(private activatedRoute:ActivatedRoute){
+  }
+  //methode hook: elle se declanche automatiquement
+  //intialiser le composant
+  ngOnInit(){
+    console.log("je suis ngOnInit");
+    //console.log(this.activatedRoute.snapshot.params['id']);
+    // la methode en dessus donne seulement le premier parametres puis pas de changement.
+    //par contre la fonction ci dessous apres chaue changement il y 'aura aussi un changement dans le clg 
+    console.log(this.activatedRoute.paramMap.subscribe(res=>{
+        console.log(res.get('id'));
+        this.id=Number(res.get('id'));
+        console.log(this.id);
+    }));
+
+  }
 
 }
